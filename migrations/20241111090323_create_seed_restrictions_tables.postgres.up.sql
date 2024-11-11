67df96b3b6811196f5e5403eeba73703283b8375
Jordan Ryano <jordanryano20@gmail.com>
2024-11-11 16:06:15 +0700

    adding a new migrations seeds

---
+++ migrations/20241111090323_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
+	 ('Reservation','2024-10-31 00:00:00.000','2024-10-31 00:00:00.000'),
+	 ('Owner Block','2024-11-02 00:00:00.000','2024-11-02 00:00:00.000');
